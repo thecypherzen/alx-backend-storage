a07c5d99117e28c9f71cfb9a267ed0b03497a3d2
William Inyam <sw.inyam@outlook.com>
2024-10-18 20:02:48 +0000

    style(0x00:7): add query comments

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -10,30 +10,33 @@
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 
-CREATE PROCEDURE ComputeAverageScoreForUser(IN p_user_id INT)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE totalScores, projectCount, subTotal INT DEFAULT 0;
     DECLARE done INT DEFAULT FALSE;
     DECLARE c_cursor CURSOR FOR
+        -- get sum of scores by project for user
         SELECT SUM(score)
         FROM holberton.corrections
-        WHERE user_id = p_user_id
+        WHERE user_id = user_id
         GROUP BY project_id;
     DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
     OPEN c_cursor;
     sumLoop: LOOP
+        -- fetch row from cursor
         FETCH c_cursor INTO subTotal;
         IF done THEN
             LEAVE sumLoop;
         END IF;
         SET projectCount = projectCount + 1;
         SET totalScores = totalScores + subTotal;
+        SELECT totalScores, projectCount, subTotal;
     END LOOP;
     CLOSE c_cursor;
-
+    -- set user's average score
     UPDATE users
         SET average_score = ROUND(totalScores/projectCount)
-        WHERE id = p_user_id;
+        WHERE id = user_id;
 END $$
 
 DELIMITER ;
