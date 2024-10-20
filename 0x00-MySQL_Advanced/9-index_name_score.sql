085a01e19f712e521c2a66b34323d130fff3ac87
William Inyam <sw.inyam@outlook.com>
2024-10-20 18:39:30 +0000

    build(0x00:9): add one letter and column index

---
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -0,0 +1,5 @@
+-- Creates  an index idx_name_first_score on the table names and the first letter of name and the
+-- Only the first letter of name AND score must be indexed
+
+CREATE INDEX idx_name_first_score
+ON names(name(1), score);
