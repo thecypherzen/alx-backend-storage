b437f6e1c4bc70246cff1bcffbb7e5ac5c8a2546
William Inyam <sw.inyam@outlook.com>
2024-10-21 17:20:19 +0000

    build(0x01:10): add update one collection and test

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Defines a function to change school topics"""
+
+
+def update_topics(mongo_collection, name, topics) -> None:
+    """Changes all topics of a school document based on the name
+
+    Params:
+       mongo_collection(pymongo.Collection): mongodb collection object
+       name(str): the school name to update
+       topids(list[str]): list of topics offered in the school
+
+    Returns:
+       None
+    """
+    mongo_collection.update_one(
+        {"name": name}, {"$set": {"topics": topics}}
+    )
