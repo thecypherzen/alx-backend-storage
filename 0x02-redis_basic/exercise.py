2902425b70a6d173a57d114a46eea0ecce5087f3
William Inyam <sw.inyam@outlook.com>
2024-10-24 04:49:03 +0000

    add get, get_str and get_int methods

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Creates the class `Cache`"""
 from redis import Redis
-from typing import Union
+from typing import Callable, Optional, Union
 from uuid import uuid4
 
 
@@ -17,10 +17,40 @@ class Cache:
         self._redis = Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int, float]) -> str:
+    def get(self, key: str,
+            fn: Optional[Callable[
+                [bytes], Union[str, bytes, int, float]
+            ]] = None) -> Union[str, bytes, int, float, None]:
+        """Fetches a value from the database and converts to
+        the correct type using <fn>
+        Params:
+           key(str): key of the item in db
+           fn(obj:callable): functino to use for conversion
+        """
+        if not key or not (data := self._redis.get(key)):
+            None
+        if not fn:
+            return data
+        return fn(data)
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """Converts a value of key to type int"""
+        if not key or not (data := self._redis.get(key)):
+            return None
+        return self.get(key, int.from_bytes)
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """Converts value of a key to type str"""
+        if not key or not (data := self._redis.get(key)):
+            return None
+        return self.get(key, lambda val: val.decode("utf-8"))
+
+    def store(self, data: Union[str, bytes, int, float]) -> str | None:
         """Generates a random key and stores the input data
         in Redis using the random key and returns the key.
         """
-        key = str(uuid4())
-        self._redis.set(key, data)
-        return key
+        if data:
+            key = str(uuid4())
+            self._redis.set(key, data)
+            return key
+        return None
