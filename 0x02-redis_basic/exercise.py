90b17823ca91fdd4d820e5858a664863a2bfa5ef
William Inyam <sw.inyam@outlook.com>
2024-10-25 12:03:09 +0000

    fix(0x02:exercise): fix many

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -14,8 +14,8 @@ class Cache:
     """
     def __init__(self):
         """Initializes instance of `Cache`"""
-        self._redis = Redis()
-        self._redis.flushdb()
+        self.__redis = Redis()
+        self.__redis.flushdb()
 
     def get(self, key: str,
             fn: Optional[Callable[
@@ -27,7 +27,7 @@ class Cache:
            key(str): key of the item in db
            fn(obj:callable): functino to use for conversion
         """
-        if not key or not (data := self._redis.get(key)):
+        if not key or not (data := self.__redis.get(key)):
             None
         if not fn:
             return data
@@ -35,13 +35,19 @@ class Cache:
 
     def get_int(self, key: str) -> Union[int, None]:
         """Converts a value of key to type int"""
-        if not key or not (data := self._redis.get(key)):
+        if not key:
+            return None
+        data: bytes = self.__redis.get(key)
+        if not data:
             return None
         return self.get(key, int.from_bytes)
 
     def get_str(self, key: str) -> Union[str, None]:
         """Converts value of a key to type str"""
-        if not key or not (data := self._redis.get(key)):
+        if not key:
+            return None
+        data: bytes = self.__redis.get(key)
+        if not data:
             return None
         return self.get(key, lambda val: val.decode("utf-8"))
 
@@ -50,7 +56,7 @@ class Cache:
         in Redis using the random key and returns the key.
         """
         if data:
-            key = str(uuid4())
-            self._redis.set(key, data)
+            key: str = str(uuid4())
+            self.__redis.set(key, data)
             return key
         return None
