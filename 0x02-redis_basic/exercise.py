89a6c99a9b7c21de71e40022e4bc9d0267894e94
William Inyam <sw.inyam@outlook.com>
2024-10-25 17:22:37 +0000

    fix(0x02:exercise): adjust annotations and private varname

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Creates the class `Cache`"""
 from redis import Redis
-from typing import Callable, Optional, Union
+from typing import Any, Callable, Optional, Union
 from uuid import uuid4
 
 
@@ -18,38 +18,38 @@ class Cache:
         self._redis.flushdb()
 
     def get(self, key: str,
-            fn: Optional[Callable[
-                [bytes], Union[str, bytes, int, float]
-            ]] = None) -> Union[str, bytes, int, float, None]:
+            fn: Optional[
+                Callable[[bytes], Union[str, int, float, bytes]]
+                ] = None) -> Union[str, bytes, int, float, None]:
         """Fetches a value from the database and converts to
         the correct type using <fn>
         Params:
            key(str): key of the item in db
            fn(obj:callable): functino to use for conversion
         """
-        if not key or not (data := self._redis.get(key)):
-            None
+        if not key:
+            return None
+        data = self._redis.get(key)
+        if not data:
+            return None
         if not fn:
             return data
         return fn(data)
 
-    def get_int(self, key: str) -> Union[int, None]:
+    def get_int(self, key: str) -> Union[int, str, float, bytes, None]:
         """Converts a value of key to type int"""
         if not key:
             return None
-        data: bytes = self.__redis.get(key)
-        if not data:
-            return None
-        return self.get(key, int.from_bytes)
+        return self.get(key, fn=lambda n: int(n))
 
-    def get_str(self, key: str) -> Union[str, None]:
+    def get_str(self, key: str) -> Union[int, str, float, bytes, None]:
         """Converts value of a key to type str"""
         if not key:
             return None
-        data: bytes = self.__redis.get(key)
+        data: bytes = self._redis.get(key)
         if not data:
             return None
-        return self.get(key, lambda val: val.decode("utf-8"))
+        return self.get(key, fn=lambda val: val.decode("utf-8"))
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Generates a random key and stores the input data
